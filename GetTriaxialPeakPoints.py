6322cb918c5914cb4d46eb75d8d3d16a854d52b1
Chengshun Shang <chengshun.shang1996@gmail.com>
2022-09-07 12:33:53 +0200

    update get_point

--- GetTriaxialPeakPoints.py
+++ GetTriaxialPeakPoints.py
@@ -9,18 +9,24 @@ shear_limit_list = [10000, 50000, 100000, 200000, 300000, 400000, 500000, 600000
 BTS_peak_points_path_and_name = os.path.join(os.getcwd(),'Triaxial_peak_points.dat')
 with open(BTS_peak_points_path_and_name, "w") as f_w_peak_points:
 
-    for sigma_limit in sigma_limit_list:
+    xy_data_file = os.path.join(os.getcwd(),'Tensile_data.txt')
+    with open(xy_data_file, "r") as f_xy_data:
+        count = 0
+        for line in f_xy_data.readlines():
+            count += 1
+            if count % 2 == 0:
+                values = [float(s) for s in line.split()]
+                sigma_limit = int(values[0])
+                shear_limit = int(values[1])
 
-        for shear_limit in shear_limit_list:
+                aim_folder_name = 'Triaxial_Sigma' + str(sigma_limit) + '_Shear' + str(shear_limit)
+                aim_path_and_name = os.path.join(os.getcwd(),'Generated_Triaxial_cases', aim_folder_name, 'G-Triaxial_Graphs', 'G-Triaxial_graph.grf')
 
-            aim_folder_name = 'Triaxial_Sigma' + str(sigma_limit) + '_Shear' + str(shear_limit)
-            aim_path_and_name = os.path.join(os.getcwd(),'Generated_Triaxial_cases', aim_folder_name, 'G-Triaxial_Graphs', 'G-Triaxial_graph.grf')
+                triaxial_data_list = []
+                with open(aim_path_and_name, 'r') as tensile_data:
+                    for line in tensile_data:
+                        values = [float(s) for s in line.split()]
+                        triaxial_data_list.append(values[1]) 
 
-            triaxial_data_list = []
-            with open(aim_path_and_name, 'r') as tensile_data:
-                for line in tensile_data:
-                    values = [float(s) for s in line.split()]
-                    triaxial_data_list.append(values[1]) 
-
-            # write BTS_peak_points.dat
-            f_w_peak_points.write(str(sigma_limit) + ' ' + str(shear_limit) + ' ' + str(max(triaxial_data_list)) + '\n')
+                # write BTS_peak_points.dat
+                f_w_peak_points.write(str(sigma_limit) + ' ' + str(shear_limit) + ' ' + str(max(triaxial_data_list)) + '\n')
