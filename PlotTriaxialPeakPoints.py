96dfd551f733d49fac9633be7d372f7e38b17711
Chengshun Shang <chengshun.shang1996@gmail.com>
2022-09-08 14:57:02 +0200

    samll

--- PlotTriaxialPeakPoints.py
+++ PlotTriaxialPeakPoints.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 levels = 50
 
 x,y,z = [],[],[]
-BTS_peak_points_path_and_name = os.path.join(os.getcwd(),'Triaxial_peak_points.dat')
+BTS_peak_points_path_and_name = os.path.join(os.getcwd(),'Triaxial_peak_points_01.dat')
 with open(BTS_peak_points_path_and_name, "r") as f_w_peak_points:
     for line in f_w_peak_points:
         values = [float(s) for s in line.split()]
@@ -16,13 +16,14 @@ with open(BTS_peak_points_path_and_name, "r") as f_w_peak_points:
 
 #plt.contourf(X, Y, Z, 20, cmap=plt.get_cmap('YlGn'))
 #cs = plt.tricontour(x, y, z, levels=levels, colors = 'white', linewidths = 0.1)
-plt.tricontour(x, y, z, levels=[2600000, 2600001], colors = 'red', linewidths = 0.5)
-plt.tricontourf(x, y, z, levels=levels, cmap='coolwarm')
+#plt.tricontour(x, y, z, levels=[2600000, 2600001], colors = 'red', linewidths = 0.5)
+#plt.tricontourf(x, y, z, levels=levels, cmap='coolwarm')
 #cs.clabel(inline=True, fmt='%d', fontsize = 'smaller', manual=true)
+plt.plot(x,z, marker = "o")
 
 plt.xlabel('Tension_limit')
-plt.ylabel('Shear_limit')
-plt.title('Triaxial strength')
+plt.ylabel('Triaxial strength')
+#plt.title('Triaxial strength')
 
-plt.colorbar()
+#plt.colorbar()
 plt.show()
