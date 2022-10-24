a89f90fb70928eb085952cb39fb87f56f2bd54a8
Chengshun Shang <chengshun.shang1996@gmail.com>
2022-10-24 16:22:43 +0200

    update

--- plotStressStrain.py
+++ plotStressStrain.py
@@ -2,25 +2,26 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-#f_list = [0.01]
-f_list = [0.1, 0.05, 0.01]
-#phi_list = [40, 50, 60]
-phi_list = [60]
-confining_stress_list = ['0.34e6', '6.89e6', '13.79e6']
+#rf_list = [0.0, 0.0001, 0.001]
+rf_list = [0.0, 0.0001,0.001]
+phi_list = [0.0, 5.0, 10.0]
+#phi_list = [5.0]
+#confining_stress_list = ['0.34e6', '6.89e6', '13.79e6']
+confining_stress_list = ['6.89e6']
 
 plt.figure(1)
 #plt.title('Hertz model')  
 plt.xlabel('Strain / %')  
 plt.ylabel('Peak strength / MPa')   
 # creat the BTS_peak_points.dat
-for f in f_list:
+for rf in rf_list:
 
     for phi in phi_list:
 
         for confining_stress in confining_stress_list:
 
             #creat new folder
-            aim_folder_name = 'Triaxial_f' + str(f) + '_Phi' + str(phi) + '_P' + str(confining_stress)
+            aim_folder_name = 'Triaxial_rf' + str(rf) + '_Phi' + str(phi) + '_P' + str(confining_stress)
             aim_path_and_name = os.path.join(os.getcwd(),'Generated_Triaxial_cases', aim_folder_name, 'G-Triaxial_Graphs', 'G-Triaxial_graph.grf')
 
             if os.path.isfile(aim_path_and_name):
@@ -59,13 +60,13 @@ with open('exp_13.79.txt', 'r') as Young_data:
         X13.append(values[0]) 
         Y13.append(values[1])
 
-plt.plot(X11, Y11, '-',color='black', label = "experiment-0.34")
+#plt.plot(X11, Y11, '-',color='black', label = "experiment-0.34")
 plt.plot(X12, Y12, '-',color='red', label = "experiment-6.89")
-plt.plot(X13, Y13, '-',color='blue', label = "experiment-13.79")
+#plt.plot(X13, Y13, '-',color='blue', label = "experiment-13.79")
 
 
-plt.xlim((0, 4))
-plt.ylim((0, 25))
+#plt.xlim((0, 4))
+#plt.ylim((0, 25))
 plt.legend(prop={'size': 8})
 plt.show()  
 
